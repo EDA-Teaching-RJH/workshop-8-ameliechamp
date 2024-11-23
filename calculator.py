44249dd4d2e0376abaff952a5053de66ab750439
ameliechamp <ac2242@kent.ac.uk>
2024-11-23 13:48:43 +0000

    fhwjkgfu

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add (a,b):
+    output = a + b
+    return output
+
+def sub (a,b):
+    output = a - b
+    return output
+
+def mul (a,b):
+    output = a * b
+    return output
+
+def div (a,b):
+    output = a / b
+    return output
+
---
+++ test_calculator.py
@@ -0,0 +1,7 @@
+import calculator
+import pytest
+
+assert calculator.add(1,1) == 2
+assert calculator.sub(2,1) == 1
+assert calculator.mul(5,5) == 25
+assert calculator.div(25,5) == 5
